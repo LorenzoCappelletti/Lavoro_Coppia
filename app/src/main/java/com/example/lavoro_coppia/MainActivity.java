ea7d8525d1e90f15633d93c9b85acfb23bd4f95e
lorenzo <lorycappe2003@gmail.com>
2021-11-25 08:48:58 +0100

    ciao

--- app/src/main/java/com/example/lavoro_coppia/MainActivity.java
+++ app/src/main/java/com/example/lavoro_coppia/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.lavoro_coppia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button btnTest;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTest = (Button)findViewById(R.id.btnTest);
+        
+    }
+
+    public void test(View v)
+    {
+        Toast t = Toast.makeText(getApplicationContext(),"messaggio",Toast.LENGTH_LONG);// tre parametri(dove viene invocato,messaggio visualizzato,durata)
+        // oggetto TOAST ha anche una gravity per posizionarlo
+        t.show();
     }
 }
